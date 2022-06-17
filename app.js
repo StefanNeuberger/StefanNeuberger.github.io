cc4c6edab61be86a6a617dae0cff3b13d74f11bb
StefanNeuberger <stef.neuberger@gmail.com>
2022-06-17 21:50:07 +0200

    documenta test

--- app.js
+++ app.js
@@ -1,60 +1,222 @@
-const rightElem = document.getElementById("right");
+const style = document.createElement("style");
+style.innerHTML = `
+      .note {
+        background-color: yellow;
+        color:red;
+      }
+    `;
+document.head.appendChild(style);
 
-// // BACKUP SVG
-// {
-//   /* <svg
-//   width="18"
-//   height="18"
-//   viewBox="0 0 18 18"
-//   fill="none"
-//   xmlns="http://www.w3.org/2000/svg"
-// >
-// <path
-//   class="wineLeft"
-//   d="M8.86491 17.9667C7.16768 17.9667 7.5 18 6 17.8523L3.38886 17.4403L2.1999 16.9607L1.85161 16.6819L1.6521 16.3085C1.6521 16.3085 1.65209 16.8613 2.9533 17.3034C2.9533 17.3034 4.53738 17.9667 8.89359 17.9667H8.95017C10.308 17.9667 11.3829 17.9114 12.2315 17.8009C11.7804 17.8523 12.2551 17.8897 11.3499 17.8897L8.86491 17.9667Z"
-//   fill="#FC1A1C"
-// />
-//   <path
-//     class="wineRight"
-//     d="M16.3048 16.2533C16.2222 16.5343 16.3048 16.2139 16.3048 16.2533C16.2222 16.5343 16.0604 16.7368 16.0604 16.7368C15.8107 16.9572 15.2864 17.2482 14.8904 17.2482C14.947 17.2482 15.0619 17.219 15.0619 17.219C16.3631 16.8873 16.3048 16.2533 16.3048 16.2533Z"
-//     fill="#FC1A1C"
-//   />
-//   <path
-//     class="wineMiddle"
-//     d="M12.288 17.801C13.8155 17.6351 14.6076 17.4141 14.8904 17.3035C14 17.5432 12.288 17.801 12.288 17.801Z"
-//     fill="#FB8D76"
-//   />
-// </svg>; */
-// }
+const circle_NA = document.querySelector(".circle_NA");
+const circle_EU = document.querySelector(".circle_EU");
+const circle_AS = document.querySelector(".circle_AS");
+const circle_SA = document.querySelector(".circle_SA");
+const circle_AF = document.querySelector(".circle_AF");
+const circle_OC = document.querySelector(".circle_OC");
 
-// console.log("working");
+const text_NA = document.querySelector(".text_NA");
+const text_EU = document.querySelector(".text_EU");
+const text_AS = document.querySelector(".text_AS");
+const text_SA = document.querySelector(".text_SA");
+const text_AF = document.querySelector(".text_AF");
+const text_OC = document.querySelector(".text_OC");
 
-// const theSVG = document.getElementById("test");
-// const right = document.getElementsByClassName("wineRight");
-// theSVG.addEventListener("click", () => {
-//   console.log("click");
-//   console.log(right);
-//   const timeline = anime.timeline({
-//     duration: 500,
-//     easing: "easeOutQuad",
+const text_year = document.querySelector(".year");
+
+// circle_NA.setAttribute("cx", "10%");f
+
+const identifierElements = {
+  NA: { circle: circle_NA, text: text_NA },
+  EU: { circle: circle_EU, text: text_EU },
+  AS: { circle: circle_AS, text: text_AS },
+  SA: { circle: circle_SA, text: text_SA },
+  AF: { circle: circle_AF, text: text_AF },
+  OC: { circle: circle_OC, text: text_OC },
+};
+
+const dataMap = {
+  1955: {
+    NA: { value: 2, cx: 36 },
+    EU: { value: 140 },
+    AS: { value: 0 },
+    SA: { value: 0, cx: 36 },
+    AF: { value: 0 },
+    OC: { value: 0 },
+  },
+  1959: {
+    NA: { value: 40, cx: 25 },
+    EU: { value: 284 },
+    AS: { value: 4 },
+    SA: { value: 1, cx: 25 },
+    AF: { value: 0 },
+    OC: { value: 0 },
+  },
+  1964: {
+    NA: { value: 39, cx: 25 },
+    EU: { value: 298 },
+    AS: { value: 9, cx: 72 },
+    SA: { value: 0, cx: 25 },
+    AF: { value: 0 },
+    OC: { value: 0, cx: 72 },
+  },
+  1968: {
+    NA: { value: 53, cx: 32 },
+    EU: { value: 90 },
+    AS: { value: 1, cx: 62 },
+    SA: { value: 3, cx: 32 },
+    AF: { value: 0 },
+    OC: { value: 0, cx: 62 },
+  },
+  1972: {
+    NA: { value: 93, cx: 28 },
+    EU: { value: 106 },
+    AS: { value: 0 },
+    SA: { value: 1, cx: 28 },
+    AF: { value: 0 },
+    OC: { value: 0 },
+  },
+  1977: {
+    NA: { value: 190, cx: 14 },
+    EU: { value: 410 },
+    AS: { value: 10, cx: 75 },
+    SA: { value: 2, cx: 14 },
+    AF: { value: 1 },
+    OC: { value: 0, cx: 75 },
+  },
+  1982: {
+    NA: { value: 60, cx: 30 },
+    EU: { value: 117 },
+    AS: { value: 0 },
+    SA: { value: 0 },
+    AF: { value: 0 },
+    OC: { value: 2 },
+  },
+  1987: {
+    NA: { value: 63, cx: 27 },
+    EU: { value: 189 },
+    AS: { value: 14, cx: 69 },
+    SA: { value: 0, cx: 27 },
+    AF: { value: 0 },
+    OC: { value: 0, cx: 69 },
+  },
+  1992: {
+    NA: { value: 60, cx: 30 },
+    EU: { value: 116 },
+    AS: { value: 9, cx: 65 },
+    SA: { value: 6, cx: 30 },
+    AF: { value: 1 },
+    OC: { value: 3, cx: 65 },
+  },
+  1997: {
+    NA: { value: 21, cx: 35 },
+    EU: { value: 81 },
+    AS: { value: 8, cx: 63 },
+    SA: { value: 5, cx: 35 },
+    AF: { value: 1 },
+    OC: { value: 1, cx: 63 },
+  },
+  2002: {
+    NA: { value: 33, cx: 35 },
+    EU: { value: 56 },
+    AS: { value: 8, cx: 62 },
+    SA: { value: 6, cx: 35 },
+    AF: { value: 9 },
+    OC: { value: 1, cx: 62 },
+  },
+  2007: {
+    NA: { value: 16, cx: 38 },
+    EU: { value: 47 },
+    AS: { value: 24, cx: 63 },
+    SA: { value: 9, cx: 38 },
+    AF: { value: 10 },
+    OC: { value: 2, cx: 63 },
+  },
+  2012: {
+    NA: { value: 16, cx: 38 },
+    EU: { value: 47 },
+    AS: { value: 24, cx: 63 },
+    SA: { value: 11, cx: 38 },
+    AF: { value: 10 },
+    OC: { value: 2, cx: 63 },
+  },
+  2017: {
+    NA: { value: 29, cx: 32 },
+    EU: { value: 132 },
+    AS: { value: 27, cx: 68 },
+    SA: { value: 20, cx: 32 },
+    AF: { value: 18 },
+    OC: { value: 5, cx: 68 },
+  },
+  2022: {
+    NA: { value: 6, cx: 42 },
+    EU: { value: 19 },
+    AS: { value: 19, cx: 60 },
+    SA: { value: 3, cx: 42 },
+    AF: { value: 12 },
+    OC: { value: 2, cx: 60 },
+  },
+};
+
+const years = Object.keys(dataMap);
+const identifiers = Object.keys(dataMap[years[0]]);
+
+// years.forEach((year, index) => {
+//   identifiers.forEach((identifier) => {
+//     // set initial radius
+//     if (index === 14) {
+//       identifierElements[identifier].circle.setAttribute(
+//         "r",
+//         Math.sqrt(dataMap[year][identifier].value)
+//       );
+//     }
 //   });
-//   // timeline.add({
-//   //   targets: ".wineRight",
-//   //   d: [{ value: wineRightPath }],
-//   // });
-//   timeline.add({
-//     targets: ".wineMiddle",
-//     d: [{ value: wineMiddlePath }],
-//   });
-//   // timeline.add({
-//   //   targets: ".wineLeft",
-//   //   d: [{ value: wineLeftPath }],
-//   // });
 // });
 
-// const wineLeftPath =
-//   "M9.61072 4.66669C7.9135 4.66669 3.72701 4.72196 1.57719 4.88777C1.57719 4.88777 1.23774 4.88776 1.12459 5.05358C1.63376 10.1386 2.59552 19.9769 2.6521 20.3085C2.6521 20.3085 2.65209 20.8613 3.9533 21.3034C3.9533 21.3034 5.53738 21.9667 9.89359 21.9667H9.95017C11.308 21.9667 12.3829 21.9114 13.2315 21.8009C13.5144 19.6453 13.5144 19.6453 13.5144 19.6453L14.6458 4.77722C12.9486 4.72195 11.1948 4.72196 10.2896 4.72196H9.61072V4.66669Z";
-// const wineRightPath =
-//   "M17.3048 20.2533C17.3614 19.9216 18.3231 10.1386 18.8323 4.99831C18.7757 4.8325 18.3797 4.83249 18.3797 4.83249C18.0403 4.83249 17.6442 4.77721 17.2482 4.77721L16.1167 19.59C16.1167 19.59 16.1167 19.59 15.8904 21.2482C15.947 21.2482 16.0036 21.1929 16.0036 21.1929C17.3048 20.8612 17.3048 20.2533 17.3048 20.2533Z";
-// const wineMiddlePath =
-//   "M13.5709 19.6454C13.5709 19.6454 13.5709 19.6454 13.288 21.801C14.8155 21.6351 15.6076 21.4141 15.8904 21.3035C16.1167 19.6454 16.1167 19.6454 16.1167 19.6454L17.2482 4.83249C16.4562 4.77722 15.6076 4.77721 14.7024 4.77721L13.5709 19.6454Z";
+// GENERATING THE STYLES
+years.forEach((year, index) => {
+  identifiers.forEach((identifier) => {
+    const keyframeNameAnimateCircleRadius = `circleTransition${identifier}-${index}`;
+    if (index + 1 < years.length) {
+      style.innerHTML = `${style.innerHTML}
+      @keyframes ${keyframeNameAnimateCircleRadius} {
+        from {
+          r: ${Math.sqrt(dataMap[year][identifier].value)};
+          cx: ${dataMap[year][identifier].cx};
+         }
+         to {
+          r: ${Math.sqrt(dataMap[years[index + 1]][identifier].value)};
+          cx: ${dataMap[years[index + 1]][identifier].cx};
+         }
+       }
+       .animateCircleRadius${identifier}${index} {
+         animation-duration: 2s;
+         animation-fill-mode: forwards;
+         animation-name: ${keyframeNameAnimateCircleRadius};
+       }
+
+       `;
+    }
+  });
+});
+
+// ADDING CLASSNAMES EVERY 2 SECONDS TO TRIGGER NEW ANIMATION.
+let counter = 0;
+const interval = setInterval(function () {
+  if (counter < years.length) {
+    text_year.innerHTML = years[counter];
+    identifiers.forEach((identifier) => {
+      // ADD CLASSNAME FOR RADIUS ANIMATION
+      identifierElements[identifier].circle.classList.add(
+        `animateCircleRadius${identifier}${counter}`
+      );
+      // CHANGE THE VALUE IN THE CIRCLE
+      identifierElements[identifier].text.innerHTML =
+        dataMap[years[counter + 1]][identifier].value;
+    });
+
+    counter++;
+  }
+  if (counter === years.length) {
+    clearInterval(interval);
+  }
+}, 2000);
